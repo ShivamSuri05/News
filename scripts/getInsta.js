34fbcb6e15070f8566f126e9108cde949f91e128
Shivam Suri <shivamsuri0503@gmail.com>
2021-07-15 03:43:57 +0530

    Updated Token

--- scripts/getInsta.js
+++ scripts/getInsta.js
@@ -3,7 +3,7 @@ let userFeed = new Instafeed({
     get: 'user',
     target: "instafeed-container",
     resolution: 'low_resolution',
-    accessToken: 'IGQVJWMTg5RHpiWEVKSWVaWHBWcWtkLWFZAdTgzRHFwbW0wb0xUNHBHMzNWYXNTWVh4T25McXVQcjlJVEMzT0kxeW5iUVh6RXoxcEJMUzBRdGljR2gzQ3lQYTdnSnhyWlIybEMyMDdIdUxfajNiaFFNQgZDZD',
+    accessToken: 'IGQVJWcjdPaXpuMTg4UG9PM2xRNVRJRTY3OVVpS3ZAjdFpITHBTay1rWWt6b0pHZAjg4LVRZAWVpnSXpoYVNjel9QdHdWMWcwbTVSLVpmaVJ3eEF4dko5d1BlR2FKRThnLWEtMUZA3cVhuN0k1X1B1TzE1NAZDZD',
 limit: 24,
 template: '<a href="{{link}}" target="_blank" ><img title="Click here to redirect to Instagram Post" src="{{image}}" class="img12" alt="Instagram Post" /></a>',
 });
