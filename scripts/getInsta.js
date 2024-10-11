b799f003a5a7f2a9be4aecc7355864b74f78d324
ShivamSuri05 <68813883+ShivamSuri05@users.noreply.github.com>
2024-10-11 15:55:52 +0200

    Update getInsta.js ig token env

--- scripts/getInsta.js
+++ scripts/getInsta.js
@@ -3,7 +3,7 @@ let userFeed = new Instafeed({
     get: 'user',
     target: "instafeed-container",
     resolution: 'low_resolution',
-    accessToken: env.IG_TKN,
+    accessToken: process.env.IG_TOKEN,
 limit: 24,
 template: '<a href="{{link}}" target="_blank" ><img title="Click here to redirect to Instagram Post" src="{{image}}" class="img12" alt="Instagram Post" /></a>',
 });
