eeb0c8bf8912a4418437b3c2b0d5fdc0f9998973
ShivamSuri05 <68813883+ShivamSuri05@users.noreply.github.com>
2024-10-12 14:15:02 +0200

    Update getInsta.js

--- scripts/getInsta.js
+++ scripts/getInsta.js
@@ -1,10 +1,17 @@
 
-let userFeed = new Instafeed({
-    get: 'user',
-    target: "instafeed-container",
-    resolution: 'low_resolution',
-    accessToken: process.env.IG_TOKEN,
-limit: 24,
-template: '<a href="{{link}}" target="_blank" ><img title="Click here to redirect to Instagram Post" src="{{image}}" class="img12" alt="Instagram Post" /></a>',
-});
-userFeed.run();
+fetch('/.netlify/functions/getToken')
+  .then(response => response.json())
+  .then(data => {
+    let userFeed = new Instafeed({
+      get: 'user',
+      target: "instafeed-container",
+      resolution: 'low_resolution',
+      accessToken: data.token,
+      limit: 24,
+      template: '<a href="{{link}}" target="_blank" ><img title="Click here to redirect to Instagram Post" src="{{image}}" class="img12" alt="Instagram Post" /></a>',
+    });
+    userFeed.run();
+  })
+  .catch(error => {
+    console.error('Error fetching Instagram token:', error);
+  });
